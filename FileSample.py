1f37539ceb2c8fed6d9e04ed9c8f58dfaf9f86e0
eazy <48152727+ek1007jp@users.noreply.github.com>
2019-03-24 08:03:52 +0900

    2019/3/24 8:03

--- FileSample.py
+++ FileSample.py
@@ -1,5 +1,9 @@
 # coding: utf-8
-with open("README.txt", "r") as f1:
-    with open("output.txt", "w") as f2:
-        for f in f1.readlines():
-            f2.write(f)
+def main_syori():
+    with open("README.txt", "r") as f1:
+        with open("output.txt", "w") as f2:
+            for f in f1.readlines():
+                f2.write(f)
+
+if __name__ == "__main__":
+    main_syori()
