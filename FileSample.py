a3c8109ba22d523ae98eda8a3e63ad5786ebc985
eazy <48152727+ek1007jp@users.noreply.github.com>
2019-03-27 20:43:03 +0900

    2019/3/27 20:42

--- FileSample.py
+++ FileSample.py
@@ -3,7 +3,7 @@ def main_syori():
     with open("README.txt", "r") as f1:
         with open("output.txt", "w") as f2:
             for f in f1.readlines():
-                f2.write(f)
+                f2.write(f) 
 
 if __name__ == "__main__":
     main_syori()
