6a87117f24ed7e3872e4b166cb7f9d095e007d0b
ek1007jp <ek1007jp@yahoo.co.jp>
2020-12-26 14:20:41 +0900

    2020/12/26 14:20

--- mainSyori.js
+++ mainSyori.js
@@ -1,5 +1,14 @@
-function mainSyori() {
-    WScript.Echo("Sample");
+function mainSyori(strName1, strName2) {
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+    var otf1 = fso.OpenTextFile(strName1, 1);
+    var otf2 = fso.OpenTextFile(strName2, 2, true);
+    while (!otf1.AtEndOfStream) {
+        var buff = otf1.ReadLine();
+        otf2.WriteLine(buff);
+    }
+    otf1.close();
+    otf2.close();
+    WScript.Echo("Finish");
 }
 
-mainSyori();
+mainSyori("README.txt", "output.txt");
